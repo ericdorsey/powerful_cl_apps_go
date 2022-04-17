65956ae8f6caf821d6874fced62fff30e0d15efe
Eric Dorsey <dorseye@gmail.com>
2022-04-17 12:03:32 -0600

    Working count lines functionality

--- main.go
+++ main.go
@@ -2,17 +2,20 @@ package main
 
 import (
     "bufio"
+    "flag"
     "fmt"
     "io"
     "os"
 )
 
-func count(r io.Reader) int {
+func count(r io.Reader, countLines bool) int {
     // scanner is used to read text from a Reader 
     scanner := bufio.NewScanner(r)
-    
-    // Define scanner split type to words 
-    scanner.Split(bufio.ScanWords)
+  
+    if !countLines { 
+        // Define scanner split type to words 
+        scanner.Split(bufio.ScanWords)
+    }
 
     // a Counter
     wc := 0
@@ -23,5 +26,10 @@ func count(r io.Reader) int {
 }
 
 func main() {
-    fmt.Println(count(os.Stdin))
+    // Define a flags -l to count lines instead of words
+    lines := flag.Bool("l", false, "Count lines")
+    flag.Parse()
+    
+    // Output count of words or lines
+    fmt.Println(count(os.Stdin, *lines))
 }
--- main_test.go
+++ main_test.go
@@ -8,7 +8,16 @@ import (
 func TestCountWords(t *testing.T) {
     b := bytes.NewBufferString("word1 word2 word3 word4\n")
     exp := 4
-    res := count(b)
+    res := count(b, false)
+    if res != exp {
+        t.Errorf("Expected %d, got %d instead.\n", exp, res)
+    }
+}
+
+func TestCountLines(t *testing.T) {
+    b := bytes.NewBufferString("word1 word2 word3\nline2\nline3 word1")
+    exp := 3
+    res := count(b, true)
     if res != exp {
         t.Errorf("Expected %d, got %d instead.\n", exp, res)
     }
